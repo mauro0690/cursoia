0346b672e5ebe92b59f12882a5e0c69a48dd0640
mauro0690 <121508908+mauro0690@users.noreply.github.com>
2025-07-08 23:03:10 -0500

    Add files via upload

---
+++ Curso_IA_JuegMaxii/package.js
@@ -0,0 +1,19 @@
+{
+  "name": "curso-ia-juegmaxii",
+  "version": "1.0.0",
+  "description": "Curso de Creación de Contenido Infantil con IA",
+  "scripts": {
+    "build:css": "tailwindcss -i ./src/input.css -o ./dist/output.css --minify",
+    "watch:css": "tailwindcss -i ./src/input.css -o ./dist/output.css --watch"
+  },
+  "keywords": [],
+  "author": "",
+  "license": "ISC",
+  "devDependencies": {
+    "tailwindcss": "^3.4.1"
+  }
+}
+"scripts": {
+  "build:css": "tailwindcss -i ./src/input.css -o ./dist/output.css --minify",
+  "watch:css": "tailwindcss -i ./src/input.css -o ./dist/output.css --watch"
+}
